ede8749391d73b6b2fed4152b6e1465a5808baf0
Marc2880 <80326844+Marc2880@users.noreply.github.com>
2021-03-15 09:42:13 +0100

    ok

---
+++ index.js
@@ -0,0 +1,8 @@
+/* TERMINALL ==============================
+CD = Change directorty
+LS = Voir dans le dossier
+mkdir = créer un dossier
+touch = créer un dossier
+========================================
+ */
+
